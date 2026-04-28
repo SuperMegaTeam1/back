b3b942299d329bd529753ea7f600aa9036d4443f
larrxii <larrybjorling@gmail.com>
2026-04-28 17:08:48 +0300

    merge fix

--- Backend.Domain/Entities/StudyGroup.cs
+++ Backend.Domain/Entities/StudyGroup.cs
@@ -7,12 +7,10 @@ using System.Threading.Tasks;
 
 namespace Backend.Domain.Entities
 {
-    public class StudyGroup : IEntityWithId, IEntityWithChangeInfo
+    public class StudyGroup : IEntityWithId
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
 
         public ICollection<Student> Students { get; set; } = new List<Student>();
         public ICollection<Lesson> Lessons { get; set; } = new List<Lesson>();
